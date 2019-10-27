d51ba4dedcb64fa225d96fcc6be23ceb46cec014
Jerry <jerry.yang.jin@outlook.com>
2019-10-27 19:19:30 +0800

    update report, gaussian quandratures experimental test

--- gaussianquadratures.mjs
+++ gaussianquadratures.mjs
@@ -1,4 +1,4 @@
-import { constant, cube, CumulativeNormalDistribution, fourth_power, GaussChebyshev2ndIntegration, GaussChebyshevIntegration, GaussGegenbauerIntegration, GaussHermiteIntegration, GaussHyperbolicIntegration, GaussLaguerreIntegration, GaussLegendreIntegration, identity, M_PI, NormalDistribution, square, TabulatedGaussLegendre } from 'https://cdn.jsdelivr.net/npm/@quantlib/ql@latest/ql.mjs';
+import { constant, cube, CumulativeNormalDistribution, fourth_power, GaussChebyshev2ndIntegration, GaussChebyshevIntegration, GaussGegenbauerIntegration, GaussHermiteIntegration, GaussHyperbolicIntegration, GaussianQuadrature, GaussLaguerreIntegration, GaussLegendreIntegration, GaussNonCentralChiSquaredPolynomial, identity, M_PI, NonCentralChiSquareDistribution, NormalDistribution, square, TabulatedGaussLegendre } from 'https://cdn.jsdelivr.net/npm/@quantlib/ql@latest/ql.mjs';
 
 const tolerance = 1.0e-4;
 
@@ -43,6 +43,19 @@ const x_inv_cosh = {
     }
 };
 
+const x_x_nonCentralChiSquared = {
+  f: (x) => {
+    return x * x * new NonCentralChiSquareDistribution(4.0, 1.0).f(x);
+  }
+};
+
+const x_sin_exp_nonCentralChiSquared = {
+  f: (x) => {
+    return x * Math.sin(0.1 * x) * Math.exp(0.3 * x) *
+        new NonCentralChiSquareDistribution(1.0, 1.0).f(x);
+  }
+};
+
 function testSingleJacobi(I) {
     testSingle(I, 'f(x) = 1', new constant(1.0), 2.0);
     testSingle(I, 'f(x) = x', new identity(), 0.0);
@@ -76,21 +89,25 @@ describe('Gaussian quadratures tests', () => {
         testSingleJacobi(new GaussChebyshev2ndIntegration(130));
         testSingleJacobi(new GaussGegenbauerIntegration(50, 0.55));
     });
+
     it('Testing Gauss-Laguerre integration...', () => {
         testSingleLaguerre(new GaussLaguerreIntegration(16));
         testSingleLaguerre(new GaussLaguerreIntegration(150, 0.01));
         testSingle(new GaussLaguerreIntegration(16, 1.0), 'f(x) = x*exp(-x)', x_inv_exp, 1.0);
         testSingle(new GaussLaguerreIntegration(32, 0.9), 'f(x) = x*exp(-x)', x_inv_exp, 1.0);
     });
+
     it('Testing Gauss-Hermite integration...', () => {
         testSingle(new GaussHermiteIntegration(16), 'f(x) = Gaussian(x)', new NormalDistribution(), 1.0);
         testSingle(new GaussHermiteIntegration(16, 0.5), 'f(x) = x*Gaussian(x)', x_normaldistribution, 0.0);
         testSingle(new GaussHermiteIntegration(64, 0.9), 'f(x) = x*x*Gaussian(x)', x_x_normaldistribution, 1.0);
     });
+
     it('Testing Gauss hyperbolic integration...', () => {
         testSingle(new GaussHyperbolicIntegration(16), 'f(x) = 1/cosh(x)', inv_cosh, M_PI);
         testSingle(new GaussHyperbolicIntegration(16), 'f(x) = x/cosh(x)', x_inv_cosh, 0.0);
     });
+
     it('Testing tabulated Gauss-Laguerre integration...', () => {
         testSingleTabulated(new constant(1.0), 'f(x) = 1', 2.0, 1.0e-13);
         testSingleTabulated(new identity(), 'f(x) = x', 0.0, 1.0e-13);
@@ -101,8 +118,24 @@ describe('Gaussian quadratures tests', () => {
 });
 
 describe('Gaussian quadratures experimental tests', () => {
-    it('Testing Gauss non-central chi-squared integration...', () => {
-    });
-    it('Testing Gauss non-central chi-squared sum of notes...', () => {
+  it('Testing Gauss non-central chi-squared integration...', () => {
+        testSingle(new GaussianQuadrature(2, new GaussNonCentralChiSquaredPolynomial(4.0, 1.0)), 'f(x) = x^2 * nonCentralChiSquared(4, 1)(x)', x_x_nonCentralChiSquared, 37.0);
+        testSingle(new GaussianQuadrature(14, new GaussNonCentralChiSquaredPolynomial(1.0, 1.0)), 'f(x) = x * sin(0.1*x)*exp(0.3*x)*nonCentralChiSquared(1, 1)(x)', x_sin_exp_nonCentralChiSquared, 17.408092);
+  });
+
+  it('Testing Gauss non-central chi-squared sum of notes...', () => {
+        const expected = [
+            47.53491786730293, 70.6103295419633383, 98.0593406849441607,
+            129.853401537905341, 165.96963582663912, 206.389183233992043
+        ];
+        const nu = 4.0;
+        const lambda = 1.0;
+        const orthPoly = new GaussNonCentralChiSquaredPolynomial(nu, lambda);
+        const tol = 1e-5;
+        for (let n = 4; n < 10; ++n) {
+            const x = new GaussianQuadrature(n, orthPoly).x();
+            const calculated = x.reduce((p, c) => p + c, 0);
+            expect(Math.abs(calculated - expected[n - 4])).toBeLessThan(tol);
+        }
     });
 });
