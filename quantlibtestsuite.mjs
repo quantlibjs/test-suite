d72c8054228bd80537e895c05d8a36f401c0cd37
Jerry <jerry.yang.jin@outlook.com>
2019-10-28 10:50:34 +0800

    update quantlibtestsuite

--- quantlibtestsuite.mjs
+++ quantlibtestsuite.mjs
@@ -1,5 +1,5 @@
-import { Settings } from 'https://cdn.jsdelivr.net/npm/@quantlib/ql@latest/ql.mjs';
-export function configure(evaluationDate = new Date('16-September-2015')) {
+import { DateExt, Settings } from 'https://cdn.jsdelivr.net/npm/@quantlib/ql@latest/ql.mjs';
+export function configure(evaluationDate = DateExt.UTC('16,September,2015')) {
     Settings.evaluationDate.set(evaluationDate);
 }
 configure();
